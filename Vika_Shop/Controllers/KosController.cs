7daae64ee906607d561335a9929de9edac842d13
viktoryews1997@mail.ru <viktoryews1997@mail.ru>
2021-07-13 13:13:35 +0300

    * com 1

--- Vika_Shop/Controllers/KosController.cs
+++ Vika_Shop/Controllers/KosController.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Web.Mvc;
 
 namespace Vika_Shop.Controllers
 {
-	public class KosController: System.Web.Mvc.Controller
+	public class KosController: Controller
 	{
         public IActionResult Index()
         {
-            return (IActionResult)View();
+            return View();
         }
     }
 }
