512dafa6230cf3b1797e88b95c1c6959f9d25c8a
viktoryews1997@mail.ru <viktoryews1997@mail.ru>
2021-07-13 13:05:49 +0300

    * Init

---
+++ Vika_Shop/Controllers/KosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web.Mvc;
+
+namespace Vika_Shop.Controllers
+{
+	public class KosController: System.Web.Mvc.Controller
+	{
+        public IActionResult Index()
+        {
+            return (IActionResult)View();
+        }
+    }
+}
+
---
+++ Vika_Shop/Views/Kos/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vika_Shop.Views.Kos
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
